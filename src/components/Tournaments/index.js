e02420ed0111b88d0b1c8ad261717a31438e4202
Pablo Juncos <pablojuncos@wengan.com.ar>
2021-05-18 03:14:33 -0300

    Implement Tournament creation

--- src/components/Tournaments/index.js
+++ src/components/Tournaments/index.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Add, Refresh } from '@material-ui/icons';
-import { Grid, Avatar, Paper } from '@material-ui/core';
+import { Grid, Avatar, Paper, Button, TextField } from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
 import { Typography, BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import axios from 'axios';
-import { URL } from '../../config';
+import { URL, DEFAULT_VALUES } from '../../config';
+import { useDispatch } from "react-redux";
+import { showError } from '../../redux/actions'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -21,38 +24,119 @@ const useStyles = makeStyles((theme) => ({
 export const TournamentsGrid = () => {
     const classes = useStyles();
     const [list, setList] = useState([]);
+    const [cant, setCant] = useState(DEFAULT_VALUES.TEAMS_CANT);
+    const [name, setName] = useState('');
+    const [error, setError] = useState(false);
+    const [items, setItems] = useState([]);
+    const [openDialog, setOpenDialog] = React.useState(false);
+    const dispatch = useDispatch();
+    const handleClickOpenDialog = () => {
+        setError(false);
+        setCant(DEFAULT_VALUES.TEAMS_CANT);
+        setName('');
+        setOpenDialog(true);
+    };
 
-    const fetchData = async () => {
-        const { data } = await axios.get(URL.TOURNAMENT.LIST);
-        console.log(data);
-        setList(data);
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+
+    const handleSubmitDialog = () => {
+        if (isNaN(cant) || cant < 2 || !name) {
+            setError(true);
+        } else {
+            setOpenDialog(false);
+            axios.post(URL.TOURNAMENT.CREATE, {cant, name})
+                .then(() => fetchData())
+                .catch(e => dispatch(showError(e.message)));
+        }
+    };
+
+    const handleChangeCant = (event) => {
+        setCant(event.target.value);
+    };
+
+    const handleChangeName = (event) => {
+        setName(event.target.value);
+    };
+
+    const fetchData = () => {
+        axios.get(URL.TOURNAMENT.LIST)
+            .then(response => setList(response.data))
+            .catch(e => dispatch(showError(e.message)));
     };
 
     useEffect(() => {
         fetchData();
     }, []);
 
+    useEffect(() => {
+        const array = []
+        list.forEach(t=>{
+            array.push(
+            <Paper className={classes.paper}  key={t.id} variant="outlined">
+                <Grid container wrap="nowrap" spacing={2}>
+                    <Grid item>
+                        <Avatar>{t.id}</Avatar>
+                    </Grid>
+                    <Grid item xs>
+                        <Typography>{t.tournament_name}</Typography>
+                    </Grid>
+                </Grid>
+            </Paper>
+        )});
+        setItems(array);
+    }, [list]);
+
     return (
         <>
             <div style={{ height: 400, width: '100%' }}>
                 <div className={classes.root}>
-                    {list.map(t=>(
-                    <Paper className={classes.paper}  key={t.id} variant="outlined">
+                    { items.length ? items : <Paper className={classes.paper}variant="outlined">
                         <Grid container wrap="nowrap" spacing={2}>
-                            <Grid item>
-                                <Avatar>{t.id}</Avatar>
-                            </Grid>
                             <Grid item xs>
-                                <Typography>{t.tournament_name}</Typography>
+                                <Typography>No existen torneos creados</Typography>
                             </Grid>
                         </Grid>
-                    </Paper>
-                    ))}
+                    </Paper> }
                 </div>
                 <BottomNavigation showLabels>
                     <BottomNavigationAction label="Refrescar" icon={<Refresh />} onClick={fetchData}/>
+                    <BottomNavigationAction label="Nuevo torneo" icon={<Add />} onClick={handleClickOpenDialog}/>
                 </BottomNavigation>
             </div>
+            <Dialog open={openDialog}>
+                <DialogTitle>Crear torneo</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="cantidad"
+                        label="Cantidad de Equipos"
+                        error={error}
+                        onChange={handleChangeCant}
+                        variant="outlined"
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        id="name"
+                        label="Nombre del torneo"
+                        error={error}
+                        onChange={handleChangeName}
+                        variant="outlined"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleSubmitDialog} color="primary">
+                        Crear
+                    </Button>
+                    <Button onClick={handleCloseDialog} color="primary">
+                        Cancelar
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 }
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,11 @@
 const API_URL = 'http://localhost:4000/v1';
 export const URL = {
     TOURNAMENT: {
-        LIST: `${API_URL}/tournaments`
+        LIST: `${API_URL}/tournaments`,
+        CREATE: `${API_URL}/tournaments`,
     }
+}
+
+export const DEFAULT_VALUES = {
+    TEAMS_CANT: 6
 }
